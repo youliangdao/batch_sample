1a68d32c1348a1418ea3037a651e38795d497ea7
youliangdao <youliangdaojing5@gmail.com>
2022-12-08 14:14:58 +0900

    🚨test: Userモデルのテストコードを記述

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#total_score' do
+    let(:user1) { create(:user) }
+
+    context 'user_scoresテーブルにデータがある場合' do
+      before do
+        create(:user_score, user: user1, score: 4)
+        create(:user_score, user: user1, score: 5)
+        create(:user_score, user: user1, score: 6)
+      end
+
+      it 'スコアの合計を取得できる' do
+        expect(user1.total_score).to eq 15
+      end
+    end
+
+    context 'user_scoresテーブルにデータがない場合' do
+      it 'スコアの合計は0である' do
+        expect(user1.total_score).to eq 0
+      end
+    end
+
+  end
+end
