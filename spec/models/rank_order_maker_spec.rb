4fcf66841bd303b479f8a3f0ee14f795add11418
youliangdao <youliangdaojing5@gmail.com>
2022-12-09 13:28:33 +0900

    🚨test: model specの追加

---
+++ spec/models/rank_order_maker_spec.rb
@@ -0,0 +1,100 @@
+require 'rails_helper'
+
+RSpec.describe RankOrderMaker, type: :model do
+  describe '#each_ranked_user' do
+    let(:user1) { create(:user) }
+    let(:user2) { create(:user) }
+    let(:user3) { create(:user) }
+
+    context "同じ合計スコアのユーザーが存在しない場合" do
+
+      before do
+        create(:user_score, user: user1, score: 4)
+        create(:user_score, user: user1, score: 5)
+        create(:user_score, user: user1, score: 6)
+
+        create(:user_score, user: user2, score: 7)
+        create(:user_score, user: user2, score: 8)
+        create(:user_score, user: user2, score: 9)
+
+        create(:user_score, user: user3, score: 10)
+        create(:user_score, user: user3, score: 11)
+        create(:user_score, user: user3, score: 12)
+      end
+
+      it 'スコアの高い順(降順)にuserとrankを取得できる' do
+        orders = {}
+
+        RankOrderMaker.new.each_ranked_user do |user, rank|
+          orders[user.id] = rank
+        end
+
+        expect(orders.size).to eq 3
+        expect(orders[user3.id]).to eq 1
+        expect(orders[user2.id]).to eq 2
+        expect(orders[user1.id]).to eq 3
+      end
+    end
+
+    context "同じ合計スコアのユーザーが複数存在した場合" do
+      before do
+        create(:user_score, user: user1, score: 4)
+        create(:user_score, user: user1, score: 5)
+        create(:user_score, user: user1, score: 6)
+
+        create(:user_score, user: user2, score: 4)
+        create(:user_score, user: user2, score: 5)
+        create(:user_score, user: user2, score: 6)
+
+        create(:user_score, user: user3, score: 10)
+        create(:user_score, user: user3, score: 11)
+        create(:user_score, user: user3, score: 12)
+      end
+
+      it 'スコアの高い順(降順)にuserとrankを取得できる' do
+        orders = {}
+
+        RankOrderMaker.new.each_ranked_user do |user, rank|
+          orders[user.id] = rank
+        end
+
+        expect(orders.size).to eq 3
+        expect(orders[user3.id]).to eq 1
+        expect(orders[user2.id]).to eq 2
+        expect(orders[user1.id]).to eq 2
+      end
+    end
+
+    context "スコアを獲得していないユーザーが存在する場合" do
+      before do
+        create(:user_score, user: user1, score: 4)
+        create(:user_score, user: user1, score: 5)
+        create(:user_score, user: user1, score: 6)
+
+        create(:user_score, user: user2, score: 7)
+        create(:user_score, user: user2, score: 8)
+        create(:user_score, user: user2, score: 9)
+
+        create(:user_score, user: user3, score: 10)
+        create(:user_score, user: user3, score: 11)
+        create(:user_score, user: user3, score: 12)
+
+        # スコアを獲得していないユーザーを作成する
+        create(:user)
+      end
+
+      it 'スコアの高い順(降順)にuserとrankを取得できる' do
+        orders = {}
+
+        RankOrderMaker.new.each_ranked_user do |user, rank|
+          orders[user.id] = rank
+        end
+
+        expect(orders.size).to eq 3
+        expect(orders[user3.id]).to eq 1
+        expect(orders[user2.id]).to eq 2
+        expect(orders[user1.id]).to eq 3
+      end
+    end
+  end
+end
---
+++ spec/models/ranks_updater_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe RanksUpdater, type: :model do
+  describe '#update_all' do
+    let(:user1) { create(:user) }
+    let(:user2) { create(:user) }
+    let(:user3) { create(:user) }
+
+    before do
+      create(:user_score, user: user1, score: 3)
+      create(:user_score, user: user1, score: 2)
+      create(:user_score, user: user1, score: 1)
+
+      create(:user_score, user: user2, score: 10)
+      create(:user_score, user: user2, score: 3)
+      create(:user_score, user: user2, score: 4)
+
+      create(:user_score, user: user3, score: 1)
+      create(:user_score, user: user3, score: 1)
+      create(:user_score, user: user3, score: 1)
+    end
+
+    shared_examples 'ランキング情報更新処理の検証' do
+      it 'ranksテーブルにデータが作成される' do
+        RanksUpdater.new.update_all
+
+        expect(Rank.count).to eq 3
+
+        ranks = Rank.all.order(:rank)
+        expect(ranks[0].user_id).to eq user2.id
+        expect(ranks[0].rank).to eq 1
+        expect(ranks[0].score).to eq 17
+
+        expect(ranks[1].user_id).to eq user1.id
+        expect(ranks[1].rank).to eq 2
+        expect(ranks[1].score).to eq 6
+
+        expect(ranks[2].user_id).to eq user3.id
+        expect(ranks[2].rank).to eq 3
+        expect(ranks[2].score).to eq 3
+      end
+    end
+
+    context 'ranksテーブルにまだデータが存在していない場合' do
+      include_examples 'ランキング情報更新処理の検証'
+    end
+
+    context 'ranksテーブルにすでにデータが存在している場合' do
+      before do
+        create(:rank, user: user1, rank: 3, score: 10)
+        create(:rank, user: user2, rank: 2, score: 20)
+        create(:rank, user: user3, rank: 1, score: 31)
+      end
+
+      include_examples 'ランキング情報更新処理の検証'
+    end
+  end
+end
