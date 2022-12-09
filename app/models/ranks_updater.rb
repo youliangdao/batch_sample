f15546ccee5fb894f7aa898899369ff95e56cb80
youliangdao <youliangdaojing5@gmail.com>
2022-12-09 14:05:13 +0900

    ✨feat: バッチ処理にトランザクションを使用する

--- app/models/ranks_updater.rb
+++ app/models/ranks_updater.rb
@@ -1,10 +1,12 @@
 class RanksUpdater
   def update_all
-    # 現在のランキング情報をリセット
-    Rank.delete_all
+    Rank.transaction do
+      # 現在のランキング情報をリセット
+      Rank.delete_all
 
-    # ユーザーごとのスコア合計を降順に並べ替え、そこからランキング情報を再作成する
-    create_ranks
+      # ユーザーごとのスコア合計を降順に並べ替え、そこからランキング情報を再作成する
+      create_ranks
+    end
   end
 
   private
